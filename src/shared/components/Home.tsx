9cd4aeb4fa6538c5db7b8e1b21c89d8dafa0814c
jose-cleiton <cleitons835>
2022-10-01 11:23:25 -0300

    feat: Instalando @mui/material

--- src/shared/components/Home.tsx
+++ src/shared/components/Home.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@mui/material";
+
 
 export const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <p>{<Button>Teste</Button>}</p>
         </div>
     );
 }
